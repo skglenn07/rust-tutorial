80168971399fc1e09b34572382cd4a45abe0e1de
Sarah <sarahglenn04@gmail.com>
2025-01-27 02:41:20 +0000

    Completed tutorial instructions

---
+++ hello-comp423/target/debug/deps/hello_comp423-41d6e06355c1d48b.d
@@ -0,0 +1,5 @@
+/workspaces/rust-tutorial/hello-comp423/target/debug/deps/hello_comp423-41d6e06355c1d48b: src/main.rs
+
+/workspaces/rust-tutorial/hello-comp423/target/debug/deps/hello_comp423-41d6e06355c1d48b.d: src/main.rs
+
+src/main.rs:
---
+++ hello-comp423/target/debug/hello-comp423.d
@@ -0,0 +1,1 @@
+/workspaces/rust-tutorial/hello-comp423/target/debug/hello-comp423: /workspaces/rust-tutorial/hello-comp423/src/main.rs
